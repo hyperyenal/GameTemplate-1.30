6365a402f260b5043dc54c3fa5dcf786f6f253ea
Yenal Gogebakan <yenal.gogebakan@hypernova-tech.com>
2023-01-17 22:38:32 +0300

    20230117 22:38 from ubuntu

--- Assets/Scripts/TestScripts/FireBaseTests.cs
+++ Assets/Scripts/TestScripts/FireBaseTests.cs
@@ -1,12 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Firebase;
+using Firebase.Analytics;
 using UnityEngine;
 
 public class FireBaseTests
 {
+    static Firebase.FirebaseApp app;
+
     public static void InitFireBase()
     {
         Debug.Log("Firebase Init started");
+        Firebase
+            .FirebaseApp
+            .CheckAndFixDependenciesAsync()
+            .ContinueWith(task =>
+            {
+                var dependencyStatus = task.Result;
+                if (dependencyStatus == Firebase.DependencyStatus.Available)
+                {
+                    // Create and hold a reference to your FirebaseApp,
+                    // where app is a Firebase.FirebaseApp property of your application class.
+                    app = Firebase.FirebaseApp.DefaultInstance;
+                    FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+
+                    // Set a flag here to indicate whether Firebase is ready to use by your app.
+                }
+                else
+                {
+                    UnityEngine
+                        .Debug
+                        .LogError(System
+                            .String
+                            .Format("Could not resolve all Firebase dependencies: {0}",
+                            dependencyStatus));
+                    // Firebase Unity SDK is not safe to use here.
+                }
+            });
     }
 
     public static void DummyLogMessage()
